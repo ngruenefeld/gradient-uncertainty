8b7cd4cba4c3714feec82de15b158d68f12dc2ec
Nils Grünefeld <nils.gruenefeld@gmail.com>
2025-04-04 00:17:06 +0200

    qa

--- qa.sh
+++ qa.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #
-#SBATCH --job-name=gruenefeld-test
-#SBATCH --comment="Testing SLURM"
+#SBATCH --job-name=gruenefeld-ma-qa
+#SBATCH --comment="Running the QA script"
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=N.Gruenefeld@campus.lmu.de
-#SBATCH --chdir=/home/g/gruenefeld/ma
-#SBATCH --output=/home/g/gruenefeld/ma/slurm.%j.%N.out
+#SBATCH --chdir=/home/g/gruenefeld/Documents/GitHub/gradient-uncertainty
+#SBATCH --output=/home/g/gruenefeld/Documents/GitHub/gradient-uncertainty/slurm-outputs/slurm.%j.%N.out
 #SBATCH --ntasks=1
 
 source env/bin/activate
