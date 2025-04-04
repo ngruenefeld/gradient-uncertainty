aea651f4a438a6f01ad7a2d62af4f0d831002bd1
Nils Grünefeld <nils.gruenefeld@gmail.com>
2025-04-04 22:28:01 +0200

    test

--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,21 @@
+import argparse
 from utils.utils import get_response
 
-print(get_response)
+
+def main():
+    parser = argparse.ArgumentParser(description="Process some arguments.")
+    parser.add_argument("--name", type=str, help="Your name", required=True)
+    parser.add_argument("--age", type=int, help="Your age", required=False)
+
+    args = parser.parse_args()
+
+    print(f"Name: {args.name}")
+    if args.age:
+        print(f"Age: {args.age}")
+
+    # Example usage of get_response
+    print(get_response)
+
+
+if __name__ == "__main__":
+    main()
---
+++ test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+#SBATCH --job-name=gruenefeld-ma-qa
+#SBATCH --comment="Running the QA script"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=N.Gruenefeld@campus.lmu.de
+#SBATCH --chdir=/home/g/gruenefeld/Documents/GitHub/gradient-uncertainty
+#SBATCH --output=/home/g/gruenefeld/Documents/GitHub/gradient-uncertainty/slurm-outputs/slurm.%j.%N.out
+#SBATCH --ntasks=1
+
+source env/bin/activate
+python -um scripts.test --name %j
+deactivate
