5b8f3875ae8a822d1a885c107257677fe4f24a3a
Nils Grünefeld <nils.gruenefeld@gmail.com>
2025-04-08 19:08:58 +0200

    test

--- scripts/test.py
+++ scripts/test.py
@@ -1,9 +1,13 @@
 from utils.utils import get_response
+import torch
 
 
 def main():
     print("Test")
     print(get_response)
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Device:", device)
 
 
 if __name__ == "__main__":
