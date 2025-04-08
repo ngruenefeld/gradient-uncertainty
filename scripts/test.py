b08efd62007bb0dbdfc95ca406376fa98837f2f0
Nils Grünefeld <nils.gruenefeld@gmail.com>
2025-04-08 19:11:15 +0200

    test

--- scripts/test.py
+++ scripts/test.py
@@ -9,6 +9,17 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("Device:", device)
 
+    if device.type == "cuda":
+        t = torch.cuda.get_device_properties(0).total_memory
+        r = torch.cuda.memory_reserved(0)
+        a = torch.cuda.memory_allocated(0)
+        f = r - a  # free inside reserved
+
+        print("Total memory:", t)
+        print("Reserved memory:", r)
+        print("Allocated memory:", a)
+        print("Free memory:", f)
+
 
 if __name__ == "__main__":
     main()
