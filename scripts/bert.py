352b521857a0aa7c0826ab0f89062ab720964cff
Nils Grünefeld <nils.gruenefeld@gmail.com>
2025-04-18 15:29:46 +0200

    test

--- scripts/bert.py
+++ scripts/bert.py
@@ -85,6 +85,7 @@ def get_embedding(text, model):
         last_hidden = outputs.last_hidden_state
         print("Last Hidden Shape", last_hidden.shape)
         pooled = last_hidden.mean(dim=1)
+        print("Pooled Shape", pooled.shape)
     return pooled.squeeze()
 
 
