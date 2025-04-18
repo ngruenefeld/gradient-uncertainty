7d1d1e7623c443328034d109567a199ae2207c9f
Nils Grünefeld <nils.gruenefeld@gmail.com>
2025-04-18 15:54:05 +0200

    test

--- scripts/bert.py
+++ scripts/bert.py
@@ -107,6 +107,7 @@ inputs = tokenizer("This is a test", return_tensors="pt")
 print("Input Shape", inputs.input_ids.shape)
 print(inputs.input_ids)
 labels = torch.ones([1, 6], dtype=torch.long)
+labels = inputs.input_ids
 
 outputs = model(**inputs, labels=labels)
 loss = outputs.loss
