22c361fbcbb54b8b9abb99b4d47fa972dac75c07
Nils Grünefeld <nils.gruenefeld@gmail.com>
2025-04-26 17:27:23 +0200

    pip script

---
+++ shell-scripts/installs.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+#SBATCH --job-name=gruenefeld-ma-qa
+#SBATCH --comment="Pip Installs"
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=N.Gruenefeld@campus.lmu.de
+#SBATCH --chdir=/home/g/gruenefeld/Documents/GitHub/gradient-uncertainty
+#SBATCH --output=/home/g/gruenefeld/Documents/GitHub/gradient-uncertainty/slurm-outputs/slurm.%j.%N.out
+#SBATCH --ntasks=1
+
+source env/bin/activate
+pip install -U pip wheel
+pip install -U torch
+deactivate
+git add .
+git commit -m "pip installs"
+git push
