cb6ce80ea39974238bdbedbd24eb4e5c6940ca68
Nils Grünefeld <nils.gruenefeld@gmail.com>
2025-05-02 12:43:10 +0200

    test

--- scripts/bert.py
+++ scripts/bert.py
@@ -62,16 +62,16 @@ def process_test_samples(
                     inputs, tokenizer, device, replacement_prob=0.9
                 )
                 labels = synonym_inputs.input_ids.clone().to(device)
-                # print(f"Original: {inputs}")
-                print(
-                    f"Original: {tokenizer.decode(inputs['input_ids'], skip_special_tokens=True)}"
-                )
-                # print(f"Synonyms: {synonym_inputs}")
-                print(
-                    f"Synonyms: {tokenizer.decode(synonym_inputs['input_ids'], skip_special_tokens=True)}"
-                )
-                # print(f"Labels: {labels}")
-                print(f"Labels: {tokenizer.decode(labels, skip_special_tokens=True)}")
+                print(f"Original: {inputs}")
+                # print(
+                #     f"Original: {tokenizer.decode(inputs['input_ids'], skip_special_tokens=True)}"
+                # )
+                print(f"Synonyms: {synonym_inputs}")
+                # print(
+                #     f"Synonyms: {tokenizer.decode(synonym_inputs['input_ids'], skip_special_tokens=True)}"
+                # )
+                print(f"Labels: {labels}")
+                # print(f"Labels: {tokenizer.decode(labels, skip_special_tokens=True)}")
             elif counterfactual == "identity":
                 labels = inputs.input_ids.clone().to(device)
             else:
