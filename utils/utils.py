fe1d2b8ba47b8c35631a739f7da84283efc8c9a8
Nils Grünefeld <nils.gruenefeld@gmail.com>
2025-05-02 12:53:41 +0200

    test

--- utils/utils.py
+++ utils/utils.py
@@ -404,7 +404,10 @@ def replace_tokens_with_synonyms(inputs, tokenizer, device, replacement_prob=0.1
                 if synonym_tokens["input_ids"].shape[1] == 1:
                     print("Replacing token")
                     print(synonym_tokens["input_ids"][0, 0])
+                    print()
                     input_ids[i, j] = synonym_tokens["input_ids"][0, 0]
 
+    print(inputs)
     inputs["input_ids"] = input_ids
+    print(inputs)
     return inputs
