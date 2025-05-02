1f4a9768037bc7891dc720eaeee4485f0bf2c30f
Nils Grünefeld <nils.gruenefeld@gmail.com>
2025-05-02 12:56:31 +0200

    test

--- scripts/bert.py
+++ scripts/bert.py
@@ -62,14 +62,14 @@ def process_test_samples(
                     inputs, tokenizer, device, replacement_prob=0.9
                 )
                 labels = synonym_inputs.input_ids.clone().to(device)
-                # # print(f"Original: {inputs}")
-                # print(
-                #     f"Original: {tokenizer.decode(inputs['input_ids'][0], skip_special_tokens=True)}"
-                # )
-                # # print(f"Synonyms: {synonym_inputs}")
-                # print(
-                #     f"Synonyms: {tokenizer.decode(synonym_inputs['input_ids'][0], skip_special_tokens=True)}"
-                # )
+                print(f"Original: {inputs}")
+                print(
+                    f"Original: {tokenizer.decode(inputs['input_ids'][0], skip_special_tokens=True)}"
+                )
+                print(f"Synonyms: {synonym_inputs}")
+                print(
+                    f"Synonyms: {tokenizer.decode(synonym_inputs['input_ids'][0], skip_special_tokens=True)}"
+                )
                 # # print(f"Labels: {labels}")
                 # print(
                 #     f"Labels: {tokenizer.decode(labels[0], skip_special_tokens=True)}"
